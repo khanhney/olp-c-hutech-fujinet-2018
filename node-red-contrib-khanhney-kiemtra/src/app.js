aa9cb599cadf31f80bd590eb0c9a65c5a6a42073
khanhney <khanhney.dev@gmail.com>
2018-06-13 12:08:54 +0700

    function custom_kiemtra

---
+++ node-red-contrib-khanhney-kiemtra/src/app.js
@@ -0,0 +1,26 @@
+module.exports = function(RED) {
+    function lineAppFunction(config) {
+        RED.nodes.createNode(this,config);
+        var context = this.context();
+        var node = this;
+        node.TEMP = config.TEMP;
+        node.EC = config.EC;
+        node.TDS = config.TDS;
+        node.HUMI = config.HUMI;
+        this.on('input', function(msg) {
+            var result = {};
+            var check_temp = false, check_ec = false, check_tds = false, check_humi = false;
+            if (node.TEMP >= 20 && node.TEMP <= 40) return check_temp = true;
+            if (node.EC >= 0.5 && node.EC <= 2.4) return check_ec = true;
+            if (node.TDS >= 280 && node.TDS <= 1400) return check_tds = true;
+            if (node.HUMI >= 40 && node.HUMI <= 80) return check_humi = true;
+            if (check_ec && check_temp && check_humi && check_tds){
+                result.payload = true;
+            }else {
+                result.payload = false;
+            }
+            node.send(result);
+        });
+    }
+    RED.nodes.registerType("KiemTra",lineAppFunction);
+};
